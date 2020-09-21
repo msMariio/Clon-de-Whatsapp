4119a6a0ac201629aa2098e4389b76af9705692e
msMariio <55538435+msMariio@users.noreply.github.com>
2020-09-21 17:27:31 +0200

    Creación Scaffolding clon de Whatsapp

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCUxlmfWoDLtbu0mW8fr7UPQ5AwNOJWOyQ",
+    authDomain: "whatsapp-ffe81.firebaseapp.com",
+    databaseURL: "https://whatsapp-ffe81.firebaseio.com",
+    projectId: "whatsapp-ffe81",
+    storageBucket: "whatsapp-ffe81.appspot.com",
+    messagingSenderId: "319129546133",
+    appId: "1:319129546133:web:b4387282dfba7722751a9c",
+    measurementId: "G-7W81L7BKSL"
+};
